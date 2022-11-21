42e8307487451ddf661d5a3615808c87f9b10613
Cotton <emiljohnllanes772@gmail.com>
2022-11-22 01:15:34 +0800

    added comments and made the time spent to a more readable format

--- entry.py
+++ entry.py
@@ -10,11 +10,14 @@ class WindowEntry():
         print(f"time started: {self.timeStarted}")
         print(f"time finished: {self.timeFinished}")
 
+        # minus the time started to time finished and convert it to a more readable format
         hours = self.timeFinished[0] - self.timeStarted[0]
         minutes = self.timeFinished[1] - self.timeStarted[1]
         seconds = self.timeFinished[2] - self.timeStarted[2]
+        timeSpent = (hours, minutes, seconds)
 
-        print(f"time spent in program : {hours}H {minutes}M {seconds}S")
+        print(
+            f"time spent in program : {timeSpent[0]}H {timeSpent[1]}M {timeSpent[2]}S")
 
 
 if __name__ == "__main__":
