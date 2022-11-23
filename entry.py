bcce2b1de3d40566ecbc485d7101fc25d94d0071
Cotton <emiljohnllanes772@gmail.com>
2022-11-23 09:36:33 +0800

    modified main.py for optimization and made window class on entry.py

--- entry.py
+++ entry.py
@@ -1,3 +1,6 @@
+import sqlite3
+import datetime as dt
+
 
 class WindowEntry():
     def __init__(self, windowTitle, timeStarted, timeFinished):
@@ -6,18 +9,31 @@ class WindowEntry():
         self.timeFinished = timeFinished
 
     def record_in_database(self):
-        print(f"window: {self.windowTitle}")
-        print(f"time started: {self.timeStarted}")
-        print(f"time finished: {self.timeFinished}")
+
+        # print(f"window: {self.windowTitle}")
+        # print(f"time started: {self.timeStarted}")
+        # print(f"time finished: {self.timeFinished}")
 
         # minus the time started to time finished and convert it to a more readable format
         hours = self.timeFinished[0] - self.timeStarted[0]
         minutes = self.timeFinished[1] - self.timeStarted[1]
         seconds = self.timeFinished[2] - self.timeStarted[2]
-        timeSpent = (hours, minutes, seconds)
+        timeElapsed = f"{hours}, {minutes}, {seconds}"
 
-        print(
-            f"time spent in program : {timeSpent[0]}H {timeSpent[1]}M {timeSpent[2]}S")
+        conn = sqlite3.connect("pyTrack.db")
+
+        c = conn.cursor()
+
+        c.execute("""
+                    INSERT INTO windowTimeEntries VALUES(?,?,?)
+
+        """, (self.windowTitle, timeElapsed, str(dt.date.today())))
+
+        conn.commit()
+
+        conn.close()
+
+        print("successfully added to database")
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -11,35 +11,61 @@ def main():
     timeFinished = None
 
     while True:
-        try:
-            # get the active window
-            newActiveWindow = gw.getActiveWindow()
-            time.sleep(5)
+        # get the active window
+        newActiveWindow = gw.getActiveWindow()
+        time.sleep(5)
 
-            # if the window changed
-            if newActiveWindow != lastActiveWindow:
+        dtNow = dt.datetime.now()
+        timeFinished = (dtNow.hour, dtNow.minute, dtNow.second)
 
-                dtNow = dt.datetime.now()
-                timeFinished = (dtNow.hour, dtNow.minute, dtNow.second)
+        # if the window changed
+        if newActiveWindow != lastActiveWindow:
+            # if there is both time started and finished this means that we changed from another window
+            # to another in this case we will record it on the database
+            if timeFinished is not None and timeStarted is not None and lastActiveWindow is not None:
+                windowEntry = entry.WindowEntry(lastActiveWindow.title,
+                                                timeStarted, timeFinished)
+                windowEntry.record_in_database()
 
-                #  if there is both time started and finished this means that we changed from another window
-                # to another in this case we will record it on the database
-                if timeFinished is not None and timeStarted is not None and lastActiveWindow is not None:
-                    windowEntry = entry.WindowEntry(lastActiveWindow.title,
-                                                    timeStarted, timeFinished)
-                    windowEntry.record_in_database()
+            # set the last active window to the current window
+            lastActiveWindow = newActiveWindow
 
-                # set the last active window to the current window
-                lastActiveWindow = newActiveWindow
+            timeStarted = (dtNow.hour, dtNow.minute, dtNow.second)
 
-                timeStarted = (dtNow.hour, dtNow.minute, dtNow.second)
+        # check if the windo/app is productive or not
+        if lastActiveWindow is not None:
+            appType = check_app_type(lastActiveWindow)
+            elapsedTime = getElapsedTime(timeStarted, timeFinished)
+            print(elapsedTime)
 
-            else:
-                pass
+        else:
+            pass
 
-        except:
-            print("error has occurred")
-            quit()
+
+def check_app_type(lastActiveWindow) -> str:
+    productiveApps = ["Visual Studio Code"]
+    badApps = ["Opera"]
+    appType = ""
+
+    seperatedTitle: list = lastActiveWindow.title.split("- ")
+    for app in productiveApps:
+        if seperatedTitle[-1].upper() == app.upper():
+            print(f"this is an productive app {app}")
+            appType = "good"
+    for app in badApps:
+        if seperatedTitle[-1].upper() == app.upper():
+            print(f"this is a bad app {app}")
+            appType = "bad"
+
+    return appType
+
+
+def getElapsedTime(timeStarted, timeFinished) -> tuple:
+    hours = timeFinished[0] - timeStarted[0]
+    minutes = timeFinished[1] - timeStarted[1]
+    seconds = timeFinished[2] - timeStarted[2]
+    timeSpent = (hours, minutes, seconds)
+    return timeSpent
 
 
 if __name__ == "__main__":
