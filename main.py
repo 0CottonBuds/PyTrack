a774c7ce48b671d469e95c9ba58ddb1cb4bc0498
Cotton <emiljohnllanes772@gmail.com>
2022-12-10 00:17:23 +0800

    refactored and converted to snake case

--- main.py
+++ main.py
@@ -5,90 +5,94 @@ import entry
 
 
 class app:
+    time_started: tuple
+    time_finished: tuple
+
     def __init__(self):
         print("helloWorld")
         self.run()
 
     # this is the main loop
     def run(self):
-        lastActiveWindow = None
+        last_active_window = None
 
-        dtNow = dt.datetime.now()
-        timeStarted = (dtNow.hour, dtNow.minute, dtNow.second)
-        timeFinished = None
+        dt_now = dt.datetime.now()
+        self.time_started = (dt_now.hour, dt_now.minute, dt_now.second)
+        self.time_finished = (0, 0, 0)
 
         while True:
             # get the active window
-            newActiveWindow = gw.getActiveWindow()
+            new_active_window = gw.getActiveWindow()
             time.sleep(5)
 
-            dtNow = dt.datetime.now()
-            timeFinished = (dtNow.hour, dtNow.minute, dtNow.second)
+            dt_now = dt.datetime.now()
+            self.time_finished = (dt_now.hour, dt_now.minute, dt_now.second)
 
             # if the window changed
-            if newActiveWindow != lastActiveWindow:
+            if new_active_window != last_active_window:
                 # if there is both time started and finished this means that we changed from another window
                 # to another in this case we will record it on the database
                 if (
-                    timeFinished is not None
-                    and timeStarted is not None
-                    and lastActiveWindow is not None
+                    self.time_finished is not None
+                    and self.time_started is not None
+                    and last_active_window is not None
                 ):
-                    windowEntry = entry.WindowEntryIn(
-                        lastActiveWindow.title, timeStarted, timeFinished
+
+                    window_entry = entry.WindowEntryIn(
+                        last_active_window.title, self.time_started, self.time_finished
                     )
-                    windowEntry.recordInDatabase()
+                    window_entry.record_in_database()
 
                 # set the last active window to the current window
-                lastActiveWindow = newActiveWindow
+                last_active_window = new_active_window
 
-                timeStarted = (dtNow.hour, dtNow.minute, dtNow.second)
+                self.time_started = (dt_now.hour, dt_now.minute, dt_now.second)
 
             # check if the window app is productive or not
-            if lastActiveWindow is not None:
-                appType = checkAppType(lastActiveWindow)
-                if timeFinished is not None and timeStarted is not None:
-                    elapsedTime = getElapsedTime(timeStarted, timeFinished)
-                    print(elapsedTime)
+            if last_active_window is not None:
+                app_type = check_app_type(last_active_window)
+                if self.time_finished is not None and self.time_started is not None:
+                    elapsed_time = self.get_elapsed_time()
+                    print(elapsed_time)
 
             else:
-                lastActiveWindow = newActiveWindow
+                last_active_window = new_active_window
+
+        # get time elapsed from time started and time finished
+
+    def get_elapsed_time(self) -> tuple:
+        hours: float = self.time_finished[0] - self.time_started[0]
+        minutes: float = self.time_finished[1] - self.time_started[1]
+        seconds: float = self.time_finished[2] - self.time_started[2]
+
+        # check if the time became negative and compensate
+        if seconds < 0:
+            minutes -= 1
+            seconds += 60
+        if minutes < 0:
+            hours -= 1
+            minutes += 60
+
+        time_spent = (hours, minutes, seconds)
+        return time_spent
 
 
-def checkAppType(lastActiveWindow) -> str:
-    productiveApps = ["Visual Studio Code"]
-    badApps = ["Opera"]
-    appType = ""
+def check_app_type(last_active_window) -> str:
+    productive_apps = ["Visual Studio Code"]
+    bad_apps = ["Opera"]
+    app_type = ""
 
-    separatedTitle: list = lastActiveWindow.title.split("- ")
-    for app in productiveApps:
-        if separatedTitle[-1].upper() == app.upper():
-            print(f"this is an productive app {str(separatedTitle)}")
-            appType = "good"
-    for app in badApps:
-        if separatedTitle[-1].upper() == app.upper():
-            print(f"this is a bad app {str(separatedTitle)}")
-            appType = "bad"
+    separated_title: list[str] = last_active_window.title.split("- ")
+    for app in productive_apps:
+        if separated_title[-1].upper() == app.upper():
+            print(f"this is an productive app {str(separated_title)}")
+            app_type = "good"
+    for app in bad_apps:
+        if separated_title[-1].upper() == app.upper():
+            print(f"this is a bad app {str(separated_title)}")
+            app_type = "bad"
 
-    return appType
-
-
-# get time elapsed from time started and time finished
-def getElapsedTime(timeStarted, timeFinished) -> tuple:
-    hours: float = timeFinished[0] - timeStarted[0]
-    minutes: float = timeFinished[1] - timeStarted[1]
-    seconds: float = timeFinished[2] - timeStarted[2]
-
-    # check if the time became negative and compensate
-    if seconds < 0:
-        minutes -= 1
-        seconds += 60
-    if minutes < 0:
-        hours -= 1
-        minutes += 60
-
-    timeSpent = (hours, minutes, seconds)
-    return timeSpent
+    return app_type
 
 
 if __name__ == "__main__":
