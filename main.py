8f4f5c57e45f9a43e24883795677f1c26a8f7b16
Cotton <emiljohnllanes772@gmail.com>
2022-12-10 22:52:55 +0800

    refactored main.py for more cohesion

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygetwindow as gw
 import time
 import datetime as dt
 import entry
-from window_type import check_app_type
+from window_type import *
 
 # from window_type import check_app_type
 
@@ -16,56 +16,80 @@ class app:
         self.main()
 
     def main(self):
-        last_active_window = None
+        self.last_active_window = None
 
-        dt_now = dt.datetime.now()
-        self.time_started = (dt_now.hour, dt_now.minute, dt_now.second)
+        self.dt_now = dt.datetime.now()
+        self.time_started = (self.dt_now.hour, self.dt_now.minute, self.dt_now.second)
         self.time_finished = (0, 0, 0)
 
         while True:
             # get the active window
-            new_active_window = gw.getActiveWindow()
+            self.new_active_window = gw.getActiveWindow()
             time.sleep(5)
 
-            dt_now = dt.datetime.now()
-            self.time_finished = (dt_now.hour, dt_now.minute, dt_now.second)
+            self.dt_now = dt.datetime.now()
+            self.time_finished = (
+                self.dt_now.hour,
+                self.dt_now.minute,
+                self.dt_now.second,
+            )
 
-            # if the window changed
-            if new_active_window != last_active_window:
-                # if there is both time started and finished this means that we changed from another window
-                # to another in this case we will record it on the database
-                if (
-                    self.time_finished is not None
-                    and self.time_started is not None
-                    and last_active_window is not None
-                ):
+            self.__check_if_window_changed()
 
-                    window_entry = entry.WindowEntryIn(
-                        last_active_window.title, self.get_elapsed_time()
-                    )
-                    window_entry.record_in_database()
+            self.__check_if_last_window_exists()
 
-                # set the last active window to the current window
-                last_active_window = new_active_window
+    def __check_if_window_changed(self):
+        """checks if window changed if it changes it records the data to the database if all prerequisite parameters exists \n \n
+        time_finished and time_started\n
+        last_active_window and new_active_window"""
 
-                self.time_started = (dt_now.hour, dt_now.minute, dt_now.second)
+        if self.new_active_window != self.last_active_window:
 
-            # check if the window app is productive or not
-            if last_active_window is not None:
-                app_type = check_app_type(last_active_window)
-                if self.time_finished is not None and self.time_started is not None:
-                    elapsed_time = self.get_elapsed_time()
-                    print(elapsed_time)
+            """if there is both time started and finished this means that we changed from another window
+            to another in this case we will record it on the database"""
+            is_parameters_complete = (
+                self.time_finished is not None
+                and self.time_started is not None
+                and self.last_active_window is not None
+                and self.new_active_window is not None
+            )
+            if is_parameters_complete:
 
-            else:
-                last_active_window = new_active_window
+                window_entry = entry.WindowEntryIn(
+                    self.last_active_window.title,
+                    self.get_elapsed_time(),
+                )
+                window_entry.record_in_database()
+
+            # set the last active window to the current window
+            self.last_active_window = self.new_active_window
+
+            self.time_started = (
+                self.dt_now.hour,
+                self.dt_now.minute,
+                self.dt_now.second,
+            )
+
+    def __check_if_last_window_exists(self):
+        """checks if last window is none if yes then set it to the new active window"""
+        if self.last_active_window is None:
+            self.last_active_window = self.new_active_window
+        else:
+            self.__printer()
+
+    def __printer(self):
+        """placeholder function to print some data in the terminal"""
+        if self.time_finished is not None and self.time_started is not None:
+            app_type = check_app_type(self.last_active_window)
+            elapsed_time = self.get_elapsed_time()
+            print(elapsed_time)
 
     def get_elapsed_time(self) -> tuple:
         """function to subtract two time(hours, minutes, seconds)\n
         returns tuple(Hours, Minutes, Seconds)\n
         check TODO to see bugs"""
 
-        # TODO: fix bug that make this function act weird when we are subtracting from another 12 hour cycle i.e.: 11pm - 12am(00:00) which results this function to make large numbers i.e.: -23,-58,-30
+        """ TODO: fix bug that make this function act weird when we are subtracting from another 12 hour cycle i.e.: 11pm - 12am(00:00) which results this function to make large numbers i.e.: -23,-58,-30"""
 
         hours: float = self.time_finished[0] - self.time_started[0]
         minutes: float = self.time_finished[1] - self.time_started[1]
