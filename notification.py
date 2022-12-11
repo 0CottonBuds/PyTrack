791845800bdaed4b4b0d59a3f945d703f85ef179
Cotton <emiljohnllanes772@gmail.com>
2022-12-11 16:54:52 +0800

    removed fils

--- notification.py
+++
@@ -1,8 +0,0 @@
-class NotificationHandler:
-    def take_a_break(self):
-        for i in range(10):
-            print("take a break")
-
-    def get_back_to_work(self):
-        for i in range(10):
-            print("get back to work")
