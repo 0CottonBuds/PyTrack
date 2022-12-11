6f503c4243c21452eab4e1a5dbd98b1adf4a8091
Cotton <emiljohnllanes772@gmail.com>
2022-12-11 21:01:10 +0800

    added windowFilter class:

--- Scripts/window_type.py
+++ Scripts/window_type.py
@@ -40,7 +40,7 @@ class WindowType:
         print(separated_window_name)
         index = input("What name would you like to put")
         print("input ")
-        return separated_window_name[index]
+        return separated_window_name[int(index)]
 
     def __str__(self) -> str:
         return f"name: {self.window_name}\ntype: {self.window_type}\nrating: {self.window_rating}"
@@ -48,6 +48,9 @@ class WindowType:
 
 class WindowTypeIn(WindowType):
     """inherits from class window type have functions to enter data in database"""
+
+    def __init__(self, window_name: str, window_type: str, window_rating: int) -> None:
+        super().__init__(window_name, window_type, window_rating)
 
     def record_in_database(self):
         """enter the data to data base"""
@@ -62,46 +65,78 @@ class WindowTypeIn(WindowType):
         print("successfully added to database")
 
 
-def find_window_on_database_by_name(query_name: str) -> WindowType:
+def find_window_on_database_by_name(query_name: str) -> WindowType | list:
     """find window on data base by name returns windowType object"""
     conn = sqlite3.connect("pyTrack.db")
     c = conn.cursor()
     c.execute("""SELECT * FROM windowTypes WHERE windowName = ?""", (query_name,))
     results = c.fetchall()
-    window = WindowType(results[0][1], results[0][2], results[0][3])
-    conn.commit()
-    conn.close()
-    return window
+    if results != []:
+        window = WindowType(results[0][1], results[0][2], results[0][3])
+        conn.commit()
+        conn.close()
+        return window
+
+    else:
+        conn.commit()
+        conn.close()
+        return results
 
 
-def filter_windows(windows) -> list:
-    """filters the windows that you want to ignore and returns a list of windows"""
-    ignored_window_names = [
-        "",
-        "Settings",
-        "Microsoft Text Input Application",
-        "Program Manager",
-        "Clock",
-        "Scenes: Untitled",
-    ]
+class WindowFilter:
+    def __init__(self, windows: list) -> None:
+        self.windows = windows
 
-    filtered_windows = []
+    def full_filter(self):
+        self.filter_ignored_windows()
+        self.filter_windows_that_are_on_database()
 
-    for window in windows:
-        splitted_title: list[str] = window.title.split("- ")
-        # print(window.title)
-        if splitted_title[-1] in ignored_window_names:
-            print(f"this window is ignored name: {window.title}")
-            pass
-        else:
+    def filter_windows_that_are_on_database(self):
+        """cycles to the list of windows(Input) and checks if there is an entry of that window"""
+        filtered_windows = []
+        for window in self.windows:
+            splitted_name = window.title.split()
+            result = None
+            for part in splitted_name:
+                result = find_window_on_database_by_name(part)
+                if result != []:
+                    break
+                else:
+                    pass
             filtered_windows.append(window)
-    return filtered_windows
+        self.windows = filtered_windows
+        return filtered_windows
+
+    def filter_ignored_windows(self) -> list:
+        """filters the windows that you want to ignore and returns a list of windows"""
+        filtered_windows = []
+        ignored_window_names = [
+            "",
+            "Settings",
+            "Microsoft Text Input Application",
+            "Program Manager",
+            "Clock",
+        ]
+
+        for window in self.windows:
+            splitted_title: list[str] = window.title.split("- ")
+            # print(window.title)
+            if splitted_title[-1] in ignored_window_names:
+                print(f"this window is ignored name: {window.title}")
+                pass
+            else:
+                filtered_windows.append(window)
+
+        self.windows = filtered_windows
+        return filtered_windows
+
+    def __str__(self) -> str:
+        return f"{self.windows}"
 
 
 if __name__ == "__main__":
 
     windows = pygetwindow.getAllWindows()
-    filtered_windows = filter_windows(windows)
+    window_filter = WindowFilter(windows)
 
-    for window in filtered_windows:
-        find_window_on_database_by_name()
+    print(find_window_on_database_by_name("test"))
