f6c17ac8740eb72f4b8204e961971d294ecb94b3
Cotton <emiljohnllanes772@gmail.com>
2022-12-31 23:16:01 +0800

    removed everything for installers

--- README.md
+++
@@ -1,8 +0,0 @@
-# pytrack
-Window tracking software with graphs, break reminder, slack reminder
-
-# requirements
-pygetwindow
-PySide6
-config parser
-sqlite3
--- UI/WindowRecordUi/window_record.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QWidget
-
-from .ui_window_record import *
-
-
-class Ui_Window_Record(QWidget, Ui_window_record):
-    def __init__(self) -> None:
-        super().__init__()
-        self.setupUi(self)
